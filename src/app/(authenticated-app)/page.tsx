ca6fb584dedb6500b42e4a9413a8767e2d06fd36
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-01-28 11:00:10 +0530

    feat(shadcn): add shadcn in project

--- src/app/(authenticated-app)/page.tsx
+++ src/app/(authenticated-app)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '~/components/ui/button'
+
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <h1>Hello there, this is Hue Harbor</h1>
+      <Button>shadcn/ui</Button>
     </main>
   )
 }

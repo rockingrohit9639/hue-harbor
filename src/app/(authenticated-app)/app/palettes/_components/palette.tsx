4cf62d5bccfb2be67be71b5cac00b355318781c3
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-02-25 21:04:56 +0530

    style(palette): update palette styles

--- src/app/(authenticated-app)/app/palettes/_components/palette.tsx
+++ src/app/(authenticated-app)/app/palettes/_components/palette.tsx
@@ -7,8 +7,8 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import { useSession } from 'next-auth/react'
 import ErrorMessage from '~/components/ui/error-message'
 import UsagePopover from '~/components/usage-popover'
-import { cn, getPaletteCdnContent } from '~/lib/utils'
-import { variablesSchema } from '~/schema/palette'
+import { getPaletteCdnContent } from '~/lib/utils'
+import { themesSchema, variablesSchema } from '~/schema/palette'
 import PaletteActions from './palette-actions'
 import AddPaletteToFavorite from './add-palette-to-favorite'
 dayjs.extend(relativeTime)
@@ -20,29 +20,68 @@ type PaletteProps = {
   blockNavigation?: boolean
 }
 
-export default function Palette({ className, style, palette, blockNavigation }: PaletteProps) {
-  const { data } = useSession()
-  const isAuthenticated = Boolean(data?.user.id)
-
-  const variablesResult = variablesSchema.safeParse(palette.variables)
+export default function Palette(props: PaletteProps) {
+  const variablesResult = variablesSchema.safeParse(props.palette.variables)
+  const themeResult = themesSchema.safeParse(props.palette.themes)
 
   if (!variablesResult.success) {
     return <ErrorMessage title="Variables are not configured!" />
   }
 
-  const colorVariables = variablesResult.data.filter((variable) => variable.type === 'color')
+  if (!themeResult.success) {
+    return <ErrorMessage title="Themes are not configured!" />
+  }
+
+  const theme = themeResult.data[0]
+
+  const colorVariables = variablesResult.data
+    .filter((variable) => variable.theme === theme?.id)
+    .filter((variable) => variable.type === 'color')
 
   if (colorVariables.length === 0 || colorVariables.length > 6) {
     return (
-      <Link
-        className={cn('flex h-40 w-full items-center justify-center rounded-md border text-3xl font-bold', className)}
-        style={{ ...style, backgroundColor: palette.backgroundColor ?? undefined }}
-        href={`/app/palettes/${palette.slug}`}
-      >
-        {palette.name}
-      </Link>
+      <PaletteShell {...props}>
+        <Link
+          className="flex h-72 w-full items-center justify-center rounded-md border text-3xl font-bold"
+          style={{ backgroundColor: props.palette.backgroundColor ?? undefined }}
+          href={props.blockNavigation ? '#' : `/app/palettes/${props.palette.slug}`}
+        >
+          {props.palette.name}
+        </Link>
+      </PaletteShell>
     )
   }
+
+  return (
+    <PaletteShell {...props}>
+      <Link href={props.blockNavigation ? '#' : `/app/palettes/${props.palette.slug}`}>
+        <div className="group relative flex h-72 flex-col overflow-hidden rounded-xl border-[0.5px]">
+          <div className="absolute bottom-0 left-0 w-full translate-y-full bg-background px-4 py-2 transition-all ease-in-out group-hover:translate-y-0">
+            {props.palette.name}
+          </div>
+
+          {colorVariables.map((variable) => (
+            <div key={variable.id} className="flex-1" style={{ backgroundColor: String(variable.value) }} />
+          ))}
+        </div>
+      </Link>
+    </PaletteShell>
+  )
+}
+
+function PaletteShell({
+  className,
+  style,
+  children,
+  palette,
+}: {
+  children: React.ReactNode
+  palette: PrismaPalette
+  className?: string
+  style?: React.CSSProperties
+}) {
+  const { data } = useSession()
+  const isAuthenticated = Boolean(data?.user.id)
 
   return (
     <div className={className} style={style}>
@@ -52,17 +91,7 @@ export default function Palette({ className, style, palette, blockNavigation }: 
         </div>
       )}
 
-      <Link href={blockNavigation ? '#' : `/app/palettes/${palette.slug}`}>
-        <div className="group relative flex h-72 flex-col overflow-hidden rounded-xl border-[0.5px]">
-          <div className="absolute bottom-0 left-0 w-full translate-y-full bg-background px-4 py-2 transition-all ease-in-out group-hover:translate-y-0">
-            {palette.name}
-          </div>
-
-          {colorVariables.map((variable) => (
-            <div key={variable.id} className="flex-1" style={{ backgroundColor: String(variable.value) }} />
-          ))}
-        </div>
-      </Link>
+      {children}
 
       <div className="mt-2 flex items-center justify-between">
         <div className="flex items-center gap-2">
